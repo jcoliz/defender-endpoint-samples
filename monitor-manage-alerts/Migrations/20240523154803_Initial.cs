81417c1798fd909c8054cad458de46c2bdda93a6
James Coliz <jcoliz@outlook.com>
2024-05-23 08:49:15 -0700

    build(monitor): dotnet ef migrations add Initial

---
+++ monitor-manage-alerts/Migrations/20240523154803_Initial.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MdeSamples.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alerts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AlertId = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: true),
+                    AlertDetermination = table.Column<string>(type: "text", nullable: true),
+                    Severity = table.Column<string>(type: "text", nullable: false),
+                    CreatedDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    Classification = table.Column<string>(type: "text", nullable: false),
+                    AssignedTo = table.Column<string>(type: "text", nullable: true),
+                    Determination = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alerts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AlertComment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Comment = table.Column<string>(type: "text", nullable: false),
+                    CreatedDateTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    CreatedByDisplayName = table.Column<string>(type: "text", nullable: false),
+                    AlertId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlertComment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AlertComment_Alerts_AlertId",
+                        column: x => x.AlertId,
+                        principalTable: "Alerts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UpdateAlertTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    SubjectId = table.Column<int>(type: "integer", nullable: false),
+                    Action = table.Column<int>(type: "integer", nullable: false),
+                    Payload = table.Column<string>(type: "text", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UpdateAlertTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UpdateAlertTasks_Alerts_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Alerts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlertComment_AlertId",
+                table: "AlertComment",
+                column: "AlertId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alerts_AlertId",
+                table: "Alerts",
+                column: "AlertId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UpdateAlertTasks_Status",
+                table: "UpdateAlertTasks",
+                column: "Status");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UpdateAlertTasks_SubjectId",
+                table: "UpdateAlertTasks",
+                column: "SubjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlertComment");
+
+            migrationBuilder.DropTable(
+                name: "UpdateAlertTasks");
+
+            migrationBuilder.DropTable(
+                name: "Alerts");
+        }
+    }
+}
